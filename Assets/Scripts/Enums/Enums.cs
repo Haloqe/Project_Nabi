a7206059c5b749a0f6bfcc9b7963f2d6db1c7e17
Sooa <xpsa0421@gmail.com>
2023-10-13 07:53:34 +0800

    feat: add singleton and enum class

---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,21 @@
+namespace Enums
+{
+    #region Ability
+    public enum AbilityMetalType
+    {
+        Copper,
+        Gold,
+    }
+    public enum AbilityType
+    {
+        Passive,
+        Active,
+    }
+    public enum AbilityState
+    {
+        Ready,
+        Active,
+        Cooldown,
+    } 
+    #endregion Ability
+}
