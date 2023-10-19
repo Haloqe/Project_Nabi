18edc509df2060e03527e6a132b9b9b2f128e482
Sooa <xpsa0421@gmail.com>
2023-10-19 15:27:41 +0900

    chore: add editorconfig

---
+++ Assets/Scripts/KoreanTest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    internal class KoreanTest
+    {
+        // 한글 보이니?
+    }
+}
