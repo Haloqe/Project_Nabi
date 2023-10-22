2f1054058c0288243a83af5f03ea6ed22111bba0
Nayoung Lim <iris61214@gmail.com>
2023-10-22 22:38:21 +0800

    Implement delayed damage system (half damage for each part of execution) for piercing chain

--- Assets/Scripts/Player/Abilities/PiercingChain.cs
+++ Assets/Scripts/Player/Abilities/PiercingChain.cs
@@ -1,17 +1,68 @@
-using Player.Abilities.Base;
-using System.Collections;
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.U2D.IK;
 
+//This skill has two parts of execution: (1)striking chain, (2)retrieving chain
+//The enemy could therefore get hit twice by the two parts of execution. 
+//And the enemy that gets hit in the first part of execution could be different from the enemy
+//that gets hit in the second exectuion. 
 public class PiercingChain : ActiveAbilityBase
 {
+    private List<int> _affectedEnemies;
+    private int _maxTargetNum;
+    public List<SDamage> temp;
+    public SDamage specificDamage;
+    public SDamageInfo halvedDamageInfo;
+
     protected override void Initialise()
     {
-        // Do initialisation for the binding of the ability
+        _affectedEnemies = new List<int>();
+        _maxTargetNum = 5;
+        specificDamage = new SDamage();
+        halvedDamageInfo = new SDamageInfo();
     }
 
     protected override void ActivateAbility()
     {
-        // Called upon activation of ability
-        // Do initialisation for the activation of the ability
+        _affectedEnemies.Clear();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") == false)
+        {
+            Debug.LogError(_data.Name_EN + " trigger with non-enemy");
+            return;
+        }
+        if (_affectedEnemies.Count >= _maxTargetNum) return;
+        if (Utility.IsObjectInList(collision.gameObject, _affectedEnemies)) return;
+
+        IDamageable target = collision.gameObject.GetComponent<IDamageable>();
+        if (target == null) return;
+
+
+        _affectedEnemies.Add(collision.gameObject.GetInstanceID());
+
+        halvedDamageInfo = _data.DamageInfo;
+        specificDamage = _data.DamageInfo.Damages[7];
+
+        //change to half amount
+        specificDamage.Amount = specificDamage.Amount / 2;
+        halvedDamageInfo.Damages[7] = specificDamage;
+
+        //first Hit 
+        _owner.DealDamage(target, halvedDamageInfo);
+
+        //After the first hit, give the enemy stun status effect
+        //TO-DO: Enemybases에서 enemy가 받을 stun 정의하고 바꾸기
+
+        //second Hit
+        _owner.DealDamage(target, halvedDamageInfo);
+
+
+
     }
 }
