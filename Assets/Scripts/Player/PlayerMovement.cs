3cac48912d7381e62915cef91384a62572e0e0e5
Zoe <flyingdonuuts@gmail.com>
2023-12-30 22:14:15 +0800

    fix: extra jumps

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -158,16 +158,15 @@ public class PlayerMovement : MonoBehaviour
 
     public void SetJump(bool value)
     {
-        Debug.Log("jump try");
-        _jumpBufferTimeCounter = _jumpBufferTime;
+        if (_jumpCounter > 1) _jumpBufferTimeCounter = _jumpBufferTime;
+
         if (_isRooted || _isAttacking) return;
         if (_coyoteTimeCounter < 0f && _jumpCounter >= 2) return;
         if (!value) return;
 
-        Debug.Log("jump executed");
         _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
         _jumpCounter += 1;
-        _jumpBufferTimeCounter -= 1;
+        _jumpBufferTimeCounter = 0;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -178,7 +177,6 @@ public class PlayerMovement : MonoBehaviour
             _jumpCounter = 0;
             _coyoteTimeCounter = _coyoteTime;
             _animator.SetBool("IsJumping", _isJumping);
-            Debug.Log("ground touched");
             if (_jumpBufferTimeCounter > 0f) SetJump(true);
         }
     }
