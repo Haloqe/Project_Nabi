6a4b6d6643259ea79f62df8fc968ce362752a47e
Sooa <xpsa0421@gmail.com>
2024-05-26 06:55:30 +0800

    fix: spider web root

--- Assets/Scripts/Enemies/Spawns/Spider_Web.cs
+++ Assets/Scripts/Enemies/Spawns/Spider_Web.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,17 @@ public class Spider_Web : MonoBehaviour
     private static float _stunDuration = 3f;
     private static float _weaknessStrength = 2f;
     private static float _weaknessDuration = 6f;
-    private static List<StatusEffectInfo> _webStatusEffectInfo = new() {
+    private AttackInfo _webAttackInfo;
+
+    private void Awake()
+    {
+        var webStatusEffectInfo = new List<StatusEffectInfo> {
             new StatusEffectInfo(EStatusEffect.Root, _stunStrength, _stunDuration),
             new StatusEffectInfo(EStatusEffect.Weakness, _weaknessStrength, _weaknessDuration)
         };
-    private AttackInfo _webAttackInfo = new(_webStatusEffectInfo);
-
+        _webAttackInfo = new AttackInfo(webStatusEffectInfo);
+    }
+    
     private void Start()
     {
         Destroy(gameObject, 6f);
